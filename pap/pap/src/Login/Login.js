bc7eef0115ed44e67c750126a47b6b0c0ebb6559
Eduardo-002 <edu.lopes.fernandes@gmail.com>
2020-03-28 14:56:55 +0000

    fc

---
+++ pap/pap/src/Login/Login.js
@@ -0,0 +1,141 @@
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+
+const firebase = require('firebase');
+
+const LoginComponent = () => {
+  const [lor,setLor] = useState(false); // lorr = left or right
+  const [name,setName] = useState(null);
+  const [lastName,setLastName] = useState(null);
+  const [email,setEmail] = useState(null);
+  const [password,setPassword] = useState(null);
+  const [confirmPassword,setConfirmPassword] = useState(null);
+
+  const history = useHistory();
+
+  const doRegist = () => {
+    // avaliate qualiti of data
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email,password)
+      .then(authRes => {
+        const userObj = {email,name,lastName}
+        firebase
+          .firestore()
+          .collection('users')
+          .doc(email)
+          .set(userObj)
+          .then(()=>{
+            history.push("/dashboard");
+        }, dbErr=>{
+          console.log(dbErr);
+      }, authErr => {
+        console.log(authErr);
+      });
+    })
+    setPassword(null);
+  }
+
+  const doLogin = async () => {
+    
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email,password)
+      .then(()=>{
+        history.push('/dashboard');
+      }, err => {
+        console.log(err);
+      })
+  }
+
+  return (
+    <Modal show={true}>
+      <Modal.Header>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Button disabled={lor} onClick={()=>setLor(true)} block>Login</Button>
+            </Col>
+            <Col>
+              <Button disabled={!lor} onClick={()=>setLor(false)} block>Register</Button>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Header>
+
+      <Modal.Body>
+        {
+          lor?
+          (<Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
+            </Form.Group>
+          </Form>) :
+
+          (<Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control onChange={(e)=>setName(e.target.value)} placeholder="First Name" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formBasicLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control onChange={(e)=>setLastName(e.target.value)} placeholder="Last Name" />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control onChange={(e)=>setConfirmPassword(e.target.value)} type="password" placeholder="Confirm Password" />
+            </Form.Group>
+          </Form>) 
+        }
+      </Modal.Body>
+
+      <Modal.Footer>
+        {
+          lor?
+          (<Button onClick={(()=>doLogin())} variant="primary" type="submit" block>
+            Login
+          </Button>) :
+          (<Button onClick={()=>doRegist()} variant="primary" type="submit" block>
+            Register
+          </Button>)
+        }
+        
+      </Modal.Footer>
+    </Modal>
+  )
+
+  
+}
+
+export default LoginComponent;
--- pap/pap/src/index.js
+++ pap/pap/src/index.js
@@ -7,14 +7,27 @@ import * as serviceWorker from './serviceWorker';
 import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 
 import HomeComponent from './Home/Home';
-import RegisterComponent from './Register/Register';
+import LoginComponent from './Login/Login';
+import DashboardComponent from './Dashboard/Dashboard'
+
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAt7jG7uuoAu9AV8pWzHnh9Izlh31lbXV0",
+  authDomain: "pap1-369f6.firebaseapp.com",
+  databaseURL: "https://pap1-369f6.firebaseio.com",
+  projectId: "pap1-369f6",
+  storageBucket: "pap1-369f6.appspot.com",
+  messagingSenderId: "699724819078",
+  appId: "1:699724819078:web:a15f45b5863546d0b6ff07"
+});
 
 const routing = (
   <Router>
     <Switch>
       <Route exact path='/'><HomeComponent/></Route>
-      <Route path='/register'><RegisterComponent/></Route>
-      <Route path='/login'>login</Route>
+      <Route path='/login'><LoginComponent/></Route>
       <Route path='/dashboard'>dashboard</Route>
       <Route>Couldn't find that page</Route>
     </Switch>
